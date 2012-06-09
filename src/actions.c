011bf839a29e484f33c70fbf88a41d0aa2f3bbff
jaacoppi <jhiltune@ulapland.fi>
2012-06-09 19:26:49 +0300

    added probabilities to random events

--- src/actions.c
+++ src/actions.c
@@ -14,18 +14,27 @@ Enemy enemy;
 
 int check_rnd_events() {
 
-	int i = rand() % 100;
-	printf("TODO: random number between 0 and 100: %d\n",i);
+	int i = rand() % 1000;
+	printf("TODO: random number between 0 and 999: %d\n",i);
 
-	/* TODO: check what dungeon level this is. go through list of random events and act */
-	/* return 1 would mean there's no fight, but a random event instead */
-	switch (i)
-	{
-		case 0:
-			return ev_old_man();
-		default:
-			return 0;
-	}
+/* calculate probabilities to random events and then switch to the proper function */
+/* this part is only meant to make switching 
+
+// TODO: write the rest of the events, adjust probabilities
+	if (i >= 0 && i <= 10) /* ==> chance is 10 out of 1000 */
+		return ev_old_man();
+
+	if (i >= 11 && i <= 20) /* ==> chance is 10 out of 1000 */
+		return ev_old_man();
+
+	if (i >= 21 && i <= 30) /* ==> chance is 10 out of 1000 */
+		return ev_old_man();
+
+	if (i >= 31 && i <= 32) /* ==> chance is 2 out of 1000 */
+		return ev_old_man();
+
+/* if there's no random event, return 0. All random events should return 1;
+return 0;
 }
 
 void create_enemy()
